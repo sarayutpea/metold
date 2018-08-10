0ce66b8eb4ca872c500f7059d06fc2c0c4d05b47
PEA <sarayut.jansoda@hotmail.com>
2018-08-10 16:24:27 +0700

    firebase init

---
+++ src/Index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="home">
+        <h3>Home</h3>
+    </div>
+</template>
+<script>
+export default {
+    name: "Home"
+}
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import firebase from 'firebase'
+
+import Home from  './Index.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+Vue.use(firebase)
+
+const routes = [
+  { path: '/', component: Home },
+];
+const router = new VueRouter(
+  { 
+    routes,
+    mode: 'history'
+   }
+);
+const firebaseConfig = {
+  apiKey: "AIzaSyBH-doxxkRr3GPy5Pf1D9nozvzgvh2c1SI",
+  authDomain: "metold-97f86.firebaseapp.com",
+  databaseURL: "https://metold-97f86.firebaseio.com",
+  projectId: "metold-97f86",
+  storageBucket: "metold-97f86.appspot.com",
+  messagingSenderId: "951727539390"
+}
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router,
 }).$mount('#app')
